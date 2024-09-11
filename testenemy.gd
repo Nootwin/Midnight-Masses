e94f671c219ac79657719c6947315843caaa0adc
Nootwin <nolanmurray06@gmail.com>
2024-09-11 18:15:05 -0400

    works on astar2d

--- testenemy.gd
+++ testenemy.gd
@@ -1,8 +1,15 @@
 extends CharacterBody2D
 
+@onready var tilemap = $"../TileMapLayer"
+var astar = AStarGrid2D.new()
+var target_position : Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	astar.region = tilemap.get_used_rect()
+	astar.cell_size = tilemap.tile_set.tile_size * Vector2i(tilemap.scale)
+	astar.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_NEVER
+	astar.update()
 	pass # Replace with function body.
 
 
@@ -11,15 +18,17 @@ func _process(delta: float) -> void:
 	pass
 	
 func _physics_process(delta: float) -> void:
-	var path = $NavigationAgent2D.get_next_path_position()
-	print(path)
-	if (path):
-		var dir = global_position.direction_to(path)
-		velocity = Vector2(2000 * delta, 2000 * delta) * dir
-		move_and_slide()
+	var start = tilemap.local_to_map(tilemap.to_local(global_position))
+	var end = tilemap.local_to_map(tilemap.to_local(target_position))
+	var path = astar.get_point_path(start, end)
+	path.remove_at(0)
+	if (not path.is_empty()):
+		var dir = Vector2(start) - path[0]
+		print(dir)
+		velocity = Vector2(-1, -1) * dir
+		move_and_collide(velocity)
 	pass
 	
 func _input(event: InputEvent) -> void:
 	if (event is InputEventMouseButton and event.is_pressed()):
-		print("here")
-		$NavigationAgent2D.target_position = get_global_mouse_position()
+		target_position = get_global_mouse_position()
